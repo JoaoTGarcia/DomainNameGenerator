a8a8664dead51fbeae1869e6f5c34361223b4121
JoaoTGarcia <joaotavaresgarcia@gmail.com>
2023-01-11 20:04:40 +0000

    Changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#the-domain-name").innerHTML = generateDomainName();
 };
+
+const generateDomainName = () => {
+  const pronoun = ["the", "our"];
+  const adj = ["great", "big"];
+  const noun = ["jogger", "racoon"];
+  const final = [".com"];
+
+  for (const pronoun = 0; i < 10; i++)
+    for (const adj = 0; i < 10; i++)
+      for (const noun = 0; i < 10; i++)
+        for (const final = 0; i < 10; i++)
+          return pronoun + " " + adj + " " + noun + " " + final;
+};
+
+console.log(generateDomainName);
